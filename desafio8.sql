25537443b3c341b6e1b881aea5008c64087bc240
Misael Bueno <misa.hbueno@gmail.com>
2021-07-22 17:01:38 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `northwind`.`desafio8` (
+  `A` INT NULL,
+  `Trybe` CHAR(10) NULL,
+  `eh` INT NULL);
+
+INSERT INTO northwind.desafio8(A, Trybe, eh)
+VALUES (5 + 6, 'de', 2 + 8);
+
+SELECT * FROM northwind.desafio8;
