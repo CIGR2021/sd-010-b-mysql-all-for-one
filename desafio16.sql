cbbe4b10da27e4f3fbfe8bcc2192b896db72a289
Abimaelr <abimaelr.albu@gmail.com>
2021-07-22 17:09:12 -0300

    feature 1

--- desafio16.sql
+++ desafio16.sql
@@ -1,3 +1,3 @@
-SELECT submitted_date as submitted_date
+SELECT submitted_date 
 FROM northwind.purchase_orders 
 WHERE submitted_date >= '2006-01-26 00:00:00' and submitted_date <= '2006-03-31 23:59:59';
