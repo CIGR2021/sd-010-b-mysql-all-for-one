289ed003acb99c02f15038b9fb4541f74bd5b250
Carlos <cemargato@gmail.com>
2021-07-22 19:12:57 -0300

    acerto

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,1 @@
 SELECT COUNT(employee_id) AS orders_count FROM northwind.orders WHERE employee_id BETWEEN (5 AND 6) AND (shipper_id = 2);
-DELETE unit_price FROM northwind.order_details WHERE UNIT < 10000
