c2baf1e432fdfce692b92ac63903bc1313dd0d71
Carlos <cemargato@gmail.com>
2021-07-22 18:54:42 -0300

    acereto 19 e desafio 25 e 26

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,2 @@
-SELECT employee_id FROM orders WHERE employee_id BETWEEN (5 AND 6) AND (shipper_id = 2);
+SELECT COUNT(employee_id) AS orders_count FROM northwind.orders WHERE employee_id BETWEEN (5 AND 6) AND (shipper_id = 2);
+DELETE unit_price FROM northwind.order_details WHERE UNIT < 10000
