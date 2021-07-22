9699336f850e4b301cd51d9f9cc168c2b1417da5
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-22 17:53:39 -0300

    solucao requisito 23 - correcao

--- desafio23.sql
+++ desafio23.sql
@@ -1,1 +1,1 @@
-UPDATE northwind.order_details SET discount = 30 WHERE unit_price <= 10.0000 and id between 30 and 40;
+UPDATE northwind.order_details SET discount = 30 WHERE unit_price < 10.0000 and id between 30 and 40;
