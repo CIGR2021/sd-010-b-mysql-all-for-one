6dd2ec42c64ccf82df462d24245848abddb418fa
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-07-22 16:18:08 -0300

    desafio 19 refeito

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-select COUNT(employee_id) from northwind.orders where employee_id in (5, 6) and shipper_id = 2;
+select COUNT(employee_id) as orders_count from northwind.orders where employee_id in (5, 6) and shipper_id = 2;
