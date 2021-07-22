8d65e7c148ad79f061603c68e4060484ccaf062d
André De Bem <andrebemferraz@gmail.com>
2021-07-22 15:38:34 -0300

    Request III

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+-- show index from northwind.products where Key_name = 'PRIMARY'; (Esse comando serve para mostrar qual coluna representa a primary key da tabela)
+
+SELECT 
+    id
+FROM
+    northwind.products;
