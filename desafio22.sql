168830c04710894e0a1a88a319766d9d4d532931
RafaMaths <rmathscoach@gmail.com>
2021-07-22 18:15:52 -0300

    REQUIREMENT 22: DONE

--- desafio22.sql
+++ desafio22.sql
@@ -1,3 +1,1 @@
--- Atualize os dados de discount do order_details para 15. (Não é necessário utilizar o SAFE UPDATE em sua query).
-
-UPDATE 
+UPDATE northwind.order_details SET discount = 15  WHERE discount = 0;
