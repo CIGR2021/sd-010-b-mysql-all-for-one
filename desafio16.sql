ffa1b94c180994860d10f576807d95c6e42d56db
root <diegoserafim1@gmail.com>
2021-07-22 22:17:41 -0300

    Desafio 17 - registros 'id' e 'supplier_id' de 'purchase_orders' onde 'supplier_id' seja 1, ou 3, ou 5, ou 7.

--- desafio16.sql
+++ desafio16.sql
@@ -1,1 +1,4 @@
 SELECT submitted_date FROM purchase_orders WHERE submitted_date BETWEEN '2006-01-26 00:00:00' AND '2006-03-31 23:59:59';
+
+-- Mostra os registros 'id' e 'supplier_id' de 'purchase_orders' onde 'supplier_id'
+--  seja 1, ou 3, ou 5, ou 7.
