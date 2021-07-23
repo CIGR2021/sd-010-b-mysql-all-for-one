3836d5e7ccfa7499ff342e4895f69716eb01fc43
Carlos Vieira <appencigr@gmail.com>
2021-07-23 13:10:00 -0300

    Adicione ao order_details uma linha com os seguintes dados: order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e inventory_id: 129. Obs.: o id deve ser incrementado automaticamente - Feito

---
+++ desafio20.sql
@@ -0,0 +1,2 @@
+SELECT northwind.order_details;
+INSERT INTO order_details (id, order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id) VALUES (DEFAULT, 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
