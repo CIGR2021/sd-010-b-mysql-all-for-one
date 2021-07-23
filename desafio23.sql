714c04f86cfe8314790dfa06e750f43c8647ad32
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-07-23 19:07:49 -0300

    Corrigindo requisito 23 e 24

--- desafio23.sql
+++ desafio23.sql
@@ -2,4 +2,4 @@ UPDATE northwind.order_details
 SET
     discount = 30
 WHERE
-    discount = 15 AND unit_price < 10;
+    unit_price < 10.0000;
--- desafio24.sql
+++ desafio24.sql
@@ -1,6 +1,6 @@
-UPDATE northwind.order_details 
-SET 
+UPDATE northwind.order_details
+SET
     discount = 45
 WHERE
-    discount = 15 AND unit_price > 10.0000
+    unit_price > 10.0000
         AND id BETWEEN 30 AND 40;
