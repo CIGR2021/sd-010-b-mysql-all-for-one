48f2a168aa7381bbad18fdd3098c24298c8a631b
aythan azevedo <aythan16@gmail.com>
2021-07-22 23:51:49 -0300

    desafios 17  recode

--- desafio17.sql
+++ desafio17.sql
@@ -1,1 +1,1 @@
-SELECT id,supplier_id FROM northwind.purchase_orders where supplier_id = 1 or supplier_id = 3 or supplier_id = 5 or supplier_id = 7;
+SELECT id,supplier_id FROM northwind.purchase_orders where supplier_id in (1,3,5,7);
