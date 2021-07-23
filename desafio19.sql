2e21a041d3066f887b3e8c3ac21e3dee2be02502
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-07-23 08:17:31 -0300

    req19 correcting

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) AS 'order_count' FROM northwind.orders WHERE employee_id = 5 OR employee_id = 6 AND shipper_id = 2;
+SELECT COUNT(*) AS 'order_count' FROM northwind.orders WHERE employee_id IN (5, 6) AND shipper_id = 2;
