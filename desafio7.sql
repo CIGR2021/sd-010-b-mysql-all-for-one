f000f309f4b5eda3e25e6754e3596fa7094fb546
Jorge Alves <alvesdjorge@gmail.com>
2021-07-22 22:56:26 -0300

    Desafios sobre filtragem de dados

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 -- 7 - Mostre apenas os ids dos 5 últimos registros da tabela products 
 -- (a ordernação deve ser baseada na coluna id).
 
-SELECT product_name, id FROM northwind.products
-ORDER BY product_name;
+SELECT id FROM northwind.products
+ORDER BY id DESC LIMIT 5;
